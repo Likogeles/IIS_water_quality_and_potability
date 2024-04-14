a04bd60c3905f64685acd870b09d1b1148987e85
Likogeles <57628609+Likogeles@users.noreply.github.com>
2024-04-14 14:57:27 +0400

    regression_check

---
+++ decision_tree.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Загрузка набора данных
+df = pd.read_csv('water_potability.csv')
+
+# Приведение к числовому формату пустых ячеек
+features = ['ph', 'Hardness', 'Solids', 'Chloramines', 'Sulfate', 'Conductivity', 'Organic_carbon', 'Trihalomethanes', 'Turbidity']
+df[features] = df[features].fillna(df[features].mean())
+
+# Выделение признаков и целевой переменной
+X = df.drop('Potability', axis=1)  # Все признаки, кроме целевой переменной
+y = df['Potability']  # Целевая переменная
+
+# Разделение данных на обучающий (0,9%) и тестовый (0,1%) наборы
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+
+# Создание и обучение модели дерева решений
+model_dt = DecisionTreeClassifier(random_state=42)
+model_dt.fit(X_train, y_train)
+
+# Создание и обучение модели случайного леса
+model_rf = RandomForestClassifier(random_state=42)
+model_rf.fit(X_train, y_train)
+
+# Прогноз по тестовым данным
+y_pred_dt = model_dt.predict(X_test)
+y_pred_rf = model_rf.predict(X_test)
+
+# Оценка качества моделей
+accuracy_dt = accuracy_score(y_test, y_pred_dt)
+accuracy_rf = accuracy_score(y_test, y_pred_rf)
+print("\033[92m\nОценки работы моделей\033[00m")
+print('Accuracy DecisionTreeClassifier: ', accuracy_dt)
+print('Accuracy RandomForestClassifier: ', accuracy_rf)
+
+# Сравнение важности характеристик моделей
+print("\033[92m\nОценки важности признаков\033[00m")
+df_features = pd.DataFrame({'Признак': X.columns, 'DT': model_dt.feature_importances_, 'RF': model_rf.feature_importances_,
+                            'Среднее': (model_dt.feature_importances_ + model_rf.feature_importances_) / 2})
+print(df_features)
+
+# График важности характеристик по лучшей модели
+plt.figure(figsize=(10,8))
+if accuracy_dt > accuracy_rf:
+    sns.barplot(x=df_features['DT'], y=df_features['Признак'])
+else:
+    sns.barplot(x=df_features['RF'], y=df_features['Признак'])
+plt.title('Ранжирование признаков')
+plt.xlabel('Значительность')
+plt.ylabel('Признак')
+plt.show()
--- main.py
+++ main.py
@@ -1,13 +1,122 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+from my_regression import scaler
+
+
+def Nan_to_zero(a):
+    if str(a) == "nan":
+        return 0
+    return a
+
+
+def my_regression(data):
+    # Подготовка данных
+    x = data.drop('Potability', axis=1)  # Все признаки, кроме целевой переменной
+    y = data['Potability']  # Целевая переменная
+
+    # Разделение на обучающий и тестовый наборы данных
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+    # Масштабирование целевой переменной
+    y_train_scaled = scaler.fit_transform(y_train.values.reshape(-1, 1)).flatten()
+    y_test_scaled = scaler.transform(y_test.values.reshape(-1, 1)).flatten()
+
+    # Обучение решающего дерева
+    tree_reg = DecisionTreeRegressor()
+    tree_reg.fit(x_train, y_train_scaled)
+
+    # Предсказание на тестовом наборе
+    y_predict_tree = tree_reg.predict(x_test)
+
+    # Рассчет метрик
+    mid_square_tree = np.round(np.sqrt(metrics.mean_squared_error(y_test_scaled, y_predict_tree)), 3)
+    coeff_determ_tree = np.round(metrics.r2_score(y_test_scaled, y_predict_tree), 2)
+
+    # Визуализация результатов
+    plt.plot(y_test_scaled, c="red", label="y тестовые")
+    plt.plot(y_predict_tree, c="green", label=f"y предсказанные\nСр^2 = {mid_square_tree}")
+    plt.legend(loc='upper right')
+    plt.title("Регрессия с решающим деревом")
+    plt.show()
+
+
+def classification(data):
+    # Определение групп (Low, Medium, High)
+    bins = [0.0, 6.0, 8.0, float('inf')]
+    group_labels = ['Acid_water', 'Slightly_alkaline_water', 'Alkaline_water']
+
+    # Разделение данных на признаки (X) и целевую переменную (y)
+    X = data[['ph']]  # pH как признак
+    y = data['Potability']  # Признак, который мы пытаемся предсказать
+
+    # Разделение данных на обучающий и тестовый наборы
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Создание модели дерева решений
+    model = DecisionTreeClassifier()
+
+    # Обучение модели на обучающем наборе данных
+    model.fit(X_train, y_train)
+
+    # Предсказание на тестовом наборе данных
+    y_pred = model.predict(X_test)
+
+    # Оценка точности модели
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Точность модели: {:.2f}%".format(accuracy * 100))
 
 
 def data_hist():
     pd.set_option('display.max_columns', None)
 
     data = pd.read_csv('water_potability.csv')
-    data.hist()
-    plt.show()
+
+    # Приведение к числовому формату пустых ячеек
+    # data['ph'] = data['ph'].apply(Nan_to_zero)
+    # data['Hardness'] = data['Hardness'].apply(Nan_to_zero)
+    # data['Solids'] = data['Solids'].apply(Nan_to_zero)
+    # data['Chloramines'] = data['Chloramines'].apply(Nan_to_zero)
+    # data['Sulfate'] = data['Sulfate'].apply(Nan_to_zero)
+    # data['Conductivity'] = data['Conductivity'].apply(Nan_to_zero)
+    # data['Organic_carbon'] = data['Organic_carbon'].apply(Nan_to_zero)
+    # data['Trihalomethanes'] = data['Trihalomethanes'].apply(Nan_to_zero)
+    # data['Turbidity'] = data['Turbidity'].apply(Nan_to_zero)
+
+    # Приведение к числовому формату пустых ячеек
+    data['ph'] = data['ph'].fillna(data['ph'].mean())
+    data['Hardness'] = data['Hardness'].fillna(data['Hardness'].mean())
+    data['Solids'] = data['Solids'].fillna(data['Solids'].mean())
+    data['Chloramines'] = data['Chloramines'].fillna(data['Chloramines'].mean())
+    data['Sulfate'] = data['Sulfate'].fillna(data['Sulfate'].mean())
+    data['Conductivity'] = data['Conductivity'].fillna(data['Conductivity'].mean())
+    data['Organic_carbon'] = data['Organic_carbon'].fillna(data['Organic_carbon'].mean())
+    data['Trihalomethanes'] = data['Trihalomethanes'].fillna(data['Trihalomethanes'].mean())
+    data['Turbidity'] = data['Turbidity'].fillna(data['Turbidity'].mean())
+
+    # Вывод данных
+    print("Данные:")
+    print(data)
+    print()
+
+    # Вывод статистики
+    print("Статистика:")
+    print(data.describe())
+    print()
+
+    my_regression(data)
+
+    # classification(data)
+
+    # Гистограммы:
+    # data.hist()
+    # plt.show()
 
 
 if __name__ == '__main__':
